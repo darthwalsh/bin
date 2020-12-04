591b4908e9c590115e9ad8673b39e15be5e058b4
Carl Walsh <darthwalsh@gmail.com>
2020-12-03 18:03:49 -0800

    add profile ps1

---
+++ mac/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,18 @@
+. (Join-Path $PSScriptRoot ".." "Microsoft.PowerShell_profile.ps1")
+
+$ENV:PATH = "~/.pyenv/shims:~/OneDrive/bin/mac:~/Library/Python/3.8/bin:$ENV:PATH"
+
+export OBJC_DISABLE_INITIALIZE_FORK_SAFETY=YES
+
+$ENV:NVM_DIR = "$HOME/.nvm"
+function nvm() {
+  $quotedArgs = ($args | ForEach-Object { "'$_'" }) -join ' '
+  
+  zsh -c "source /usr/local/opt/nvm/nvm.sh && nvm $quotedArgs && echo __PATH_AFTER__`$PATH" | Tee-Object -Variable zsh_output | Where-Object { -not ($_ -match "^__PATH_AFTER__") }
+
+  $path_after = $zsh_output | Select-String "^__PATH_AFTER__(.+)"
+  if ($path_after) {
+    $ENV:PATH = $path_after.Matches.Groups[1].Value
+  }
+}
+# nvm --version | Out-Null # Load your current nvm path
