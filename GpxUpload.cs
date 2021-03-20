b255caf557a422dc2080011d2835ba8f929c48ff
Carl Walsh <darthwalsh@gmail.com>
2021-03-20 10:21:48 -0700

    Replace GPX with a pwsh-native script

---
+++ GpxUpload.cs
@@ -0,0 +1,56 @@
+using System;
+// using System.Diagnostics;
+using System.IO;
+// using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+public class GpxUpload 
+{
+  static HttpClient client = new HttpClient();
+  public static async Task<int> Run(string path, DateTime start)
+  {
+    // https://wiki.openstreetmap.org/wiki/API_v0.6#Create:_POST_.2Fapi.2F0.6.2Fgpx.2Fcreate
+
+    using var multiContent = new MultipartFormDataContent();
+
+    using var gxp = File.OpenRead(path);
+    using var file = new StreamContent(gxp);
+    multiContent.Add(file, "file", Path.GetFileName(path));
+
+    using var description = new StringContent($"Run {start:yyyy-MM-dd} looking for paths");
+    multiContent.Add(description, "description");
+
+    using var tags = new StringContent("");
+    multiContent.Add(tags, "tags");
+
+    using var visibility = new StringContent("trackable");
+    multiContent.Add(visibility, "visibility");
+
+    using var request = new HttpRequestMessage
+    {
+      Method = HttpMethod.Post,
+      Content = multiContent,
+      RequestUri = new Uri("https://api.openstreetmap.org/api/0.6/gpx/create"),
+    };
+
+    string osmPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile), ".rtg", "osm.txt");
+    string userpass;
+    if (File.Exists(osmPath)) {
+      userpass = File.ReadAllText(osmPath);
+    } else {
+      Console.WriteLine("Type OSM username:password");
+      userpass = Console.ReadLine() ?? throw new ArgumentNullException();
+      File.WriteAllText(osmPath, userpass); 
+    }
+    string base64 = Convert.ToBase64String(System.Text.ASCIIEncoding.ASCII.GetBytes(userpass));
+    request.Headers.Authorization = new AuthenticationHeaderValue("Basic", base64);
+
+    using var response = await client.SendAsync(request);
+    response.EnsureSuccessStatusCode();
+
+    var id = await response.Content.ReadAsStringAsync();
+    return int.Parse(id);
+  }
+}
--- gpx.ps1
+++ gpx.ps1
@@ -1,16 +1,45 @@
 <#
 .SYNOPSIS
 Publish GPX from strava to OSM
+.PARAMETER Pick
+Optionally, choose from recent activities
 #>
+
+param(
+  [switch]$Pick = $false
+)
 
 $script:ErrorActionPreference = "Stop"
 Set-StrictMode -Version Latest
 
-strava stats
+if ($Pick) {
+  strava activities
+  $id = Read-Host "Paste Activity ID"
+} else {
+  $id = strava activities --index 0 --quiet
+}
 
-dotnet run --project (Join-Path (Get-Code) RunTheGlobe) -- gpx @args
+$started = [DateTime]::Now
+"Opening browser to download $id..."
 
-# Run `dotnet publish -c Release` first
-# $dll = Join-Path (Get-Code) RunTheGlobe\bin\Release\net5.0\RunTheGlobe.dll
-# dotnet $dll gpx
+Start-Process "https://www.strava.com/activities/$id/export_gpx"
 
+# Put this delay after starding download
+$details = strava activity $id -o json | ConvertFrom-Json
+
+while (!($gpxFile = Get-ChildItem ~/Downloads/*.gpx | `
+  Where-Object LastWriteTime -gt $started | `
+  Select-Object -First 1)) {
+  Start-Sleep 0.1
+}
+
+"Using OSM API to upload $gpxFile..."
+
+Add-Type -TypeDefinition (Get-Content (Join-Path $PSScriptRoot "GpxUpload.cs") -Raw)
+
+# Call a static method
+$task = [GpxUpload]::Run($gpxFile.FullName, $details.start_date_local)
+while (-not $task.AsyncWaitHandle.WaitOne(50)) { }
+$gpxID = $task.GetAwaiter().GetResult()
+
+Start-Process "https://www.openstreetmap.org/edit?gpx=$gpxID"
