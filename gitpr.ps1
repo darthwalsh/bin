b3131b6d3d1376afadb9c7711e484897b2a6a59f
Carl Walsh <darthwalsh@gmail.com>
2021-10-08 11:34:41 -0700

    gitpr fails when files are changed.
    prints commit messages to easily copy into PR description

--- gitpr.ps1
+++ gitpr.ps1
@@ -6,6 +6,14 @@ Runs git push and pr creation
 $script:ErrorActionPreference = "Stop"
 Set-StrictMode -Version Latest
 
+$status = Get-GitStatus
+if ($status.HasWorking) {
+  Write-Warning ($status.Working -join " ")
+  throw "Working tree is dirty"
+}
+
+git fetch
+git --no-pager log HEAD --not origin/develop --format=%B%n --reverse
 
 git push -u
 gh pr create --web
