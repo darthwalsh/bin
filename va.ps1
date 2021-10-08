bb513b28e13743984b63e7321ee81285feb00969
Carl Walsh <darthwalsh@gmail.com>
2021-10-08 11:33:23 -0700

    Update virtual activate to search parent, create venv
    If none found, prompts to suggest new folder (defaults to git root)

--- va.ps1
+++ va.ps1
@@ -6,9 +6,34 @@ Activate venv
 $script:ErrorActionPreference = "Stop"
 Set-StrictMode -Version Latest
 
-$envDir = gci -Directory *env*
+try {
+  deactivate
+}
+catch {
+}
 
-if (@($envDir).Count -ne 1) { throw $envDir.Name }
+
+for ($dir = gi (Get-Location); ; $dir = $dir.Parent) {
+  if (!$dir) {
+    $newEnvParent = Get-Location
+    try {
+      $gitRoot = git rev-parse --show-toplevel
+      if ($gitRoot) { $newEnvParent = $gitRoot }
+    } catch { }
+    $envDir = Join-Path $newEnvParent "env"
+    $userChoice = (Read-Host "Create venv directory [$envDir]")
+    $envDir = $userChoice ? $userChoice : $envDir
+    
+    py -m venv $envDir
+    Write-Host "Created venv directory $envDir" -ForegroundColor Green
+    break
+  }
+  # walk up the path until we find a directory that contains an env directory
+
+  $envDir = gci $dir -Directory *env*
+  if (@($envDir).Count -gt 1) { throw "Found multiple env: $($envDir.Name)" }
+  if (@($envDir).Count -eq 1) { break }
+}
 
 $activate = @(
   (Join-Path $envDir Scripts Activate.ps1),
@@ -18,3 +43,12 @@ $activate = @(
 if (@($activate).Length -ne 1) { throw $activate.Name }
 
 . $activate -prompt 'v'
+
+$requirementsTXT = Join-Path $envDir .. "requirements.txt"
+if (Test-Path $requirementsTXT) {
+  py -m pip install -q -r $requirementsTXT
+}
+
+if (Test-Path .env) {
+  Source-Anything .env
+}
