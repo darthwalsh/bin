2270b75a7f4c45e7a4d23cbe0d6b0f5fa68bb5b3
Carl Walsh <darthwalsh@gmail.com>
2023-01-08 16:13:14 -0800

    Strava cookie finder will now open the browser if needed

--- strava_cook.py
+++ strava_cook.py
@@ -1,8 +1,25 @@
 import browser_cookie3
+import webbrowser
+import time
 
-cj = browser_cookie3.chrome()
+def get_cookies():
+  cj = browser_cookie3.chrome()
+  return [c for c in cj if 'strava' in c.domain and 'CloudFront-' in c.name]
 
-st = [c for c in cj if 'strava' in c.domain and 'CloudFront-' in c.name]
+st = get_cookies()
+if 1 or not st:
+  print('No Strava cookies found. Opening heatmap')
+  print('!!!Interaction needed!!! Dismiss the modal!')
+  print()
+  webbrowser.open('https://www.strava.com/heatmap')
+  for _ in range(10):
+    time.sleep(1)
+    st = get_cookies()
+    if st: break
+  else:
+    print('Still no Strava cookies found :(')
+    exit(1)
+  
 
 print('; '.join(f'{c.name}={c.value}' for c in st))
 
