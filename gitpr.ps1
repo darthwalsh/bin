84370ae3fb55710134cf9ada2b404343e0554507
Carl Walsh <darthwalsh@gmail.com>
2023-04-12 07:47:43 -0700

    small script changes

--- gitpr.ps1
+++ gitpr.ps1
@@ -6,6 +6,9 @@ Runs git push and pr creation
 $script:ErrorActionPreference = "Stop"
 Set-StrictMode -Version Latest
 
+gh pr view --web
+$prExists = -not $LASTEXITCODE
+
 $status = Get-GitStatus
 if ($status.HasWorking) {
   Write-Warning ($status.Working -join " ")
@@ -16,4 +19,6 @@ git fetch --recurse-submodules=false
 git --no-pager log HEAD --not origin/$(Get-GitDefaultBranch) --format=%B%n --reverse
 
 git push -u
-gh pr create --web
+if (-not $prExists) {
+  gh pr create --web
+}
