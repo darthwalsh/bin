cfc5487ebdf0e7170c2667fbf7c917ba26d18c41
Carl Walsh <darthwalsh@gmail.com>
2023-04-12 07:48:12 -0700

    va parameter with name, and avoiding install

--- va.ps1
+++ va.ps1
@@ -3,6 +3,11 @@
 Activate venv
 #>
 
+param (
+  [string] $Name,
+  [switch] $NoInstall
+)
+
 $script:ErrorActionPreference = "Stop"
 Set-StrictMode -Version Latest
 
@@ -12,28 +17,33 @@ try {
 catch {
 }
 
+if ($Name) {
+  $envDir = $Name
+} else {
+  for ($dir = gi (Get-Location); ; $dir = $dir.Parent) {
+    if (!$dir) {
+      $newEnvParent = Get-Location
+      try {
+        $gitRoot = git rev-parse --show-toplevel
+        if ($gitRoot) { $newEnvParent = $gitRoot }
+      } catch { }
+      $newEnvParent = Convert-Path $newEnvParent # Convert i.e. Temp:/ path to a real filesystem path
+      $envDir = Join-Path $newEnvParent "env"
+      $userChoice = (Read-Host "Create venv directory [$envDir]")
+      $envDir = $userChoice ? $userChoice : $envDir
+      break
+    }
+    # walk up the path until we find a directory that contains an env directory
 
-for ($dir = gi (Get-Location); ; $dir = $dir.Parent) {
-  if (!$dir) {
-    $newEnvParent = Get-Location
-    try {
-      $gitRoot = git rev-parse --show-toplevel
-      if ($gitRoot) { $newEnvParent = $gitRoot }
-    } catch { }
-    $newEnvParent = Convert-Path $newEnvParent # Convert i.e. Temp:/ path to a real filesystem path
-    $envDir = Join-Path $newEnvParent "env"
-    $userChoice = (Read-Host "Create venv directory [$envDir]")
-    $envDir = $userChoice ? $userChoice : $envDir
-    
-    py -m venv $envDir
-    Write-Host "Created venv directory $envDir" -ForegroundColor Green
-    break
+    $envDir = gci $dir -Directory *env*
+    if (@($envDir).Count -gt 1) { throw "Found multiple env: $($envDir.Name)" }
+    if (@($envDir).Count -eq 1) { break }
   }
-  # walk up the path until we find a directory that contains an env directory
+}
 
-  $envDir = gci $dir -Directory *env*
-  if (@($envDir).Count -gt 1) { throw "Found multiple env: $($envDir.Name)" }
-  if (@($envDir).Count -eq 1) { break }
+if (-not (Test-Path $envDir)) {
+  py -m venv $envDir
+  Write-Host "Created venv directory $envDir" -ForegroundColor Green
 }
 
 $activate = @(
@@ -45,9 +55,13 @@ if (@($activate).Length -ne 1) { throw $activate.Name }
 
 . $activate -prompt 'v'
 
-$requirementsTXT = Join-Path $envDir .. "requirements.txt"
-if (Test-Path $requirementsTXT) {
-  py -m pip install -q -r $requirementsTXT
+if (-not $NoInstall) {
+  py -m pip install -q --upgrade pip
+
+  $requirementsTXT = Join-Path $envDir .. "requirements.txt"
+  if (Test-Path $requirementsTXT) {
+    py -m pip install -q -r $requirementsTXT
+  }
 }
 
 if (Test-Path .env) {
