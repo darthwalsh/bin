307711c3b66d29b27e8c1b1f7160cee73a914660
Carl Walsh <darthwalsh@gmail.com>
2023-06-03 07:20:39 -0700

    brew baseline

--- brewdump.ps1
+++ brewdump.ps1
@@ -8,4 +8,5 @@ Set-StrictMode -Version Latest
 
 $brewfile = Join-Path (Get-Bin) apps Brewfile
 write-host "Writing to $brewfile" -ForegroundColor Blue
+Write-Warning "TODO remove vscode https://github.com/Homebrew/homebrew-bundle/issues/1212#issuecomment-1570684920"
 brew bundle dump --no-upgrade --describe --force "--file=$brewfile"
