fe61f77714421eb1d3d4f6e65901b0e890b930f3
Carl Walsh <darthwalsh@gmail.com>
2023-06-04 08:59:33 -0700

    loop over entire inbox

--- AppScripts/InboxGroupBy.js
+++ AppScripts/InboxGroupBy.js
@@ -1,21 +1,32 @@
 function count_inbox() {
   const counts = new Map();
-  for (const thread of GmailApp.getInboxThreads()) {
-    for (const message of thread.getMessages()) {
-      let from = message.getFrom();
-      match = from.match(/<(.*@(.*))>/);
-      if (match) {
-        const [_, email, domain] = match;
-        from = domain;
+
+  // https://stackoverflow.com/a/55723614/771768
+  let startIndex = 0;
+  const maxThreads = 500;
+  let threads
+  do {
+    threads = GmailApp.getInboxThreads(startIndex, maxThreads);
+    for (const thread of threads) {
+      for (const message of thread.getMessages()) {
+        let from = message.getFrom();
+        match = from.match(/<(.*@(.*))>/);
+        if (match) {
+          const [_, email, domain] = match;
+          from = domain;
+        }
+        counts.set(from, 1 + (counts.get(from) || 0));
       }
-      counts.set(from, 1 + (counts.get(from) || 0));
     }
-  }
-  
-  const sorted = [...counts.keys()].sort((a, b) => counts.get(b) - counts.get(a))
-  for (const from of sorted.slice(0, 40)) {
-    const url = 'https://mail.google.com/mail/u/0/#search/' + encodeURIComponent('in:inbox from:' + from)
+    startIndex += maxThreads;
+  } while (threads.length == maxThreads); // If this times out on HUGE inbox, could break earlier
+
+  const sorted = [...counts.keys()].sort((a, b) => counts.get(b) - counts.get(a));
+  for (const from of sorted.slice(0, 200)) {
+    if (counts.get(from) < 5) break;
+
+    const url =
+      "https://mail.google.com/mail/u/0/#search/" + encodeURIComponent("in:inbox from:" + from);
     console.log(from, counts.get(from), url);
   }
 }
-
