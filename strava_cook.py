2529246890101f27d317a8fc0029c9cd7129efdc
Carl Walsh <darthwalsh@gmail.com>
2023-06-04 07:54:05 -0700

    Parse strava policy

--- strava_cook.py
+++ strava_cook.py
@@ -1,3 +1,4 @@
+import base64
 import json
 import os
 import subprocess
@@ -46,10 +47,17 @@ def write_to_firestore():
   cred = credentials.Certificate(cred_path)
   firebase_admin.initialize_app(cred)
   db = firestore.client()
-  
-  # schema: https://github.com/darthwalsh/RunTheGlobe/blob/4b61a5366b06f4119291fab916b54b79ca2fe128/www/cloud.js#L173
+
+  policy, = (c.value for c in st if c.name == 'CloudFront-Policy')
+  decoded = base64.b64decode(policy.replace("_", "=")).decode()
+  policy = json.loads(decoded)
+  dateLessThan = policy['Statement'][0]['Condition']['DateLessThan']['AWS:EpochTime']
+  expiration = dateLessThan * 1000
+  print("Strava cookie expiration:", time.ctime(dateLessThan))
+
+  # schema: https://github.com/darthwalsh/RunTheGlobe/blob/09a339fc0535e5dc2f877b03c29e10d27a0466ce/www/cloud.js#L175
   db.collection('users').document(str(get_user_id())).update(
-    {'stravaCookie': f'{time.time_ns() // 1000000}:{query}'}
+    {'stravaCookie': {'cookieQuery': query, 'expires': expiration}}
   )
 
   print('Wrote to Firestore')
