4d0eb75b5184dd7488638583a472b248e7fd6533
Carl Walsh <darthwalsh@gmail.com>
2023-10-14 15:00:34 -0700

    script changes

--- gitrb.ps1
+++ gitrb.ps1
@@ -3,8 +3,16 @@
 Rebase changes onto origin/develop
 #>
 
+param(
+    [switch]$ForcePush
+)
+
 $script:ErrorActionPreference = "Stop"
 Set-StrictMode -Version Latest
 
 git fetch --recurse-submodules=false
 git rebase "origin/$(Get-GitDefaultBranch)"
+
+if ($ForcePush) {
+    git push --force-with-lease --force-if-includes
+}
--- gpx.ps1
+++ gpx.ps1
@@ -3,10 +3,13 @@
 Publish GPX from strava to OSM
 .PARAMETER Pick
 Optionally, choose from recent activities
+.PARAMETER Pick
+Optionally, paste in a specific strava activity ID
 #>
 
 param(
-  [switch]$Pick = $false
+  [switch]$Pick = $false,
+  [string]$Id = $null
 )
 
 $script:ErrorActionPreference = "Stop"
@@ -14,7 +17,7 @@ Set-StrictMode -Version Latest
 
 Add-Type -TypeDefinition (Get-Content (Join-Path $PSScriptRoot "GpxUpload.cs") -Raw)
 
-if ($Pick) {
+if ($Pick -and -not $Id) {
   $u, $p = [GpxUpload]::OsmAuth() -split -split ":"
   $secpasswd = ConvertTo-SecureString $p -AsPlainText -Force
   $credential = New-Object System.Management.Automation.PSCredential ($u, $secpasswd)
@@ -30,21 +33,31 @@ if ($Pick) {
     if (!$dates[$v.Value]) { $line }
   }
   $id = Read-Host "Paste Activity ID"
+} elseif ($Id) {
+  $id = $Id
 } else {
   $id = strava activities --index 0 --quiet --per_page 1
 }
 
 $started = [DateTime]::Now
-"Opening browser to download $id..."
+"Opening browser to download Strava activity $id ..."
 
 Start-Process "https://www.strava.com/activities/$id/export_gpx"
 
 # Put this delay after starting download
 $details = strava activity $id -o json | ConvertFrom-Json
 
+
+$timeout = New-TimeSpan -Seconds 20
+$startTime = Get-Date
+
 while (!($gpxFile = Get-ChildItem ~/Downloads/*.gpx | `
   Where-Object LastWriteTime -gt $started | `
   Select-Object -First 1)) {
+  if ((Get-Date) - $startTime -ge $timeout) {
+    Write-Error "Timeout waiting for GPX file to download"
+    return
+  }
   Start-Sleep 0.1
 }
 
