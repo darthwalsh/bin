774572b832aee5d250b0f17646bd7229eed0acbc
Carl Walsh <darthwalsh@gmail.com>
2024-03-05 15:03:38 -0800

    GitDefaultBranch caches

--- Get-GitDefaultBranch.ps1
+++ Get-GitDefaultBranch.ps1
@@ -3,6 +3,10 @@
 Outputs i.e. master or main
 #>
 
+git config my.default.branch
+if (-not $LASTEXITCODE) { return }
+
 $remoteDefault = (git remote show origin) -match "HEAD " -split " " | select -Last 1
 
-git config --default $remoteDefault my.default.branch
+git config my.default.branch $remoteDefault
+$remoteDefault
