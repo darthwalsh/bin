8ea0d11eb6a6182125aeeffe2d37cf0e8de468b9
Carl Walsh <darthwalsh@gmail.com>
2024-06-03 15:57:53 -0700

    Get-GitDefaultBranch -refresh

--- Get-GitDefaultBranch.ps1
+++ Get-GitDefaultBranch.ps1
@@ -1,16 +1,24 @@
 <#
 .SYNOPSIS
 Outputs i.e. master or main
+.PARAMETER Refresh
+If specified, the default branch is refreshed from the remote.
 #>
+
+param (
+    [switch] $Refresh = $false
+)
 
 $script:ErrorActionPreference = "Stop"
 Set-StrictMode -Version Latest
 
-try {
-  git config my.default.branch
-  return
-} catch {
-  Write-Verbose "Ignoring native command error"
+if (!$Refresh) {
+  try {
+    git config my.default.branch
+    return
+  } catch {
+    Write-Verbose "Ignoring native command error"
+  }
 }
 
 $remoteDefault = (git remote show origin) -match "HEAD " -split " " | select -Last 1
