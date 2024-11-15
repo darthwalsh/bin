82585941bd6410fa23962aa72a8476e5332b093d
Carl Walsh <darthwalsh@gmail.com>
2024-11-15 14:56:49 -0800

    script to run bash on docker build

---
+++ dsh.ps1
@@ -0,0 +1,17 @@
+<#
+.SYNOPSIS
+Run Docker baSH on recent docker image
+.EXAMPLE
+PS> docker build . && dsh
+#>
+
+$script:ErrorActionPreference = "Stop"
+Set-StrictMode -Version Latest
+
+$last_tag = docker images | Select-Object -Skip 1 -First 1 | ForEach-Object { ($_ -split ' ')[0] }
+
+"Running $last_tag"
+
+$envFile = "$last_tag.env"
+$envArgs = if (Test-Path $envFile) { @("--env-file", $envFile) } else { @() }
+docker run --name $last_tag --rm -it --entrypoint /bin/bash @envArgs $last_tag @args
