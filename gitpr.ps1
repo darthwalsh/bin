f2f193ec42ce11ee4a460625579ba0f26f58d041
Carl Walsh <darthwalsh@gmail.com>
2024-12-16 16:06:08 -0800

    enhanced PR creation

--- gitpr.ps1
+++ gitpr.ps1
@@ -1,7 +1,14 @@
 <#
 .SYNOPSIS
 Runs git push and pr creation
+.PARAMETER create
+Creates the PR instead of opening browser
 #>
+
+[CmdletBinding(SupportsShouldProcess = $true)]
+param (
+  [switch]$create = $false
+)
 
 $script:ErrorActionPreference = "Stop"
 Set-StrictMode -Version Latest
@@ -32,9 +39,33 @@ $log
 $log | Set-Clipboard
 
 
-Write-Host "TODO check the branch is rebased on origin/$(Get-GitDefaultBranch)" -ForegroundColor Blue # TODO
+[int]$behind, [int]$ahead = (git rev-list --left-right --count "origin/$(Get-GitDefaultBranch)...HEAD") -split '\s'
+if ($behind) {
+  throw "Branch is behind origin/$(Get-GitDefaultBranch) by $behind commits" # TODO maybe auto-run gitrb?
+}
+
 git push -u
 
-gh pr create --web
-# MAYBE trying setting the --body $log, but I think it's not supported with --web?
-# If not using --web, then set --reviewer too based on heuristic on branch name
+if (!$create) {
+  gh pr create --web
+  return
+}
+
+$reviewer = Get-DefaultReviewer # Need to create this in your profile
+
+if ($ahead -ne 1) {
+  throw "TODO test with -WhatIf that --fill-verbose gives the right --body"
+  # MAYBE need to use --body $log, maybe need --title to split it?
+}
+
+$dry = @(if ($WhatIfPreference) { '--dry-run' })
+
+gh pr create @reviewer --fill-verbose @dry
+
+try {
+  gh pr merge --auto --squash
+} catch {
+  Write-Warning "Failed to enable auto-merge "
+}
+
+gh pr view --web
