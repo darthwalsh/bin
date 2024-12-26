23b2c2afa1f7a83dc56776d2eb9b03d00a177b4c
Carl Walsh <darthwalsh@gmail.com>
2024-12-26 04:56:19 -0800

    pipdeptree forward args

--- pipdeptree.ps1
+++ pipdeptree.ps1
@@ -6,4 +6,6 @@ Runs pipdeptree against the current venv python
 $script:ErrorActionPreference = "Stop"
 Set-StrictMode -Version Latest
 
-pipx run pipdeptree --python auto
+# On windows this script prints "⚠️  pipdeptree is already on your PATH and installed at C:\code\bin\pipdeptree.PS1. Downloading and running anyway."
+# MAYBE squash this or rename the PS1 or contribute upstream
+pipx run pipdeptree --python auto @args
