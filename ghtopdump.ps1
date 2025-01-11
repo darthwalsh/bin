5ac50f247be89302964f67a4d63b432be3de2ceb
Carl Walsh <darthwalsh@gmail.com>
2025-01-11 06:38:18 -0800

    Include more github repos, but not if they were imported to github from ancient repo

--- ghtopdump.ps1
+++ ghtopdump.ps1
@@ -9,10 +9,17 @@ Set-StrictMode -Version Latest
 $notes = '~/notes/MyNotes/Projects'
 New-Item $notes -ItemType Directory -ErrorAction SilentlyContinue | out-null
 
-foreach ($repo in gh repo list --source --json 'name,updatedAt,url' | ConvertFrom-JSON) {
+foreach ($repo in gh repo list --source --limit 100 --json 'name,owner,updatedAt,url' | ConvertFrom-JSON) {
+  if ($repo.name.StartsWith("CSS")) { continue }
+
   $file = Join-Path $notes "$($repo.name).md"
   if (Test-Path $file) { continue }
-  if ($repo.updatedAt -lt ([datetime]::now + -2d * 365)) { continue }
+
+  $oldestInclude = [datetime]::now - [TimeSpan]::FromDays(365 * 4)
+  if ($repo.updatedAt -lt $oldestInclude) { continue }
+
+  $lastCommitDate = [datetime](gh api "repos/$($repo.owner.login)/$($repo.name)/commits" --jq '.[0].commit.author.date')
+  if ($lastCommitDate -lt $oldestInclude) { continue }
   Set-Content $file "[github]($($repo.url))`n"
   $file
 }
