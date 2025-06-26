7d1d9a0c602ab4ce75382d3eaa5f69714b9fbd72
Carl Walsh <darthwalsh@gmail.com>
2025-06-26 16:13:19 -0700

    gpx pick now filters

--- gpx.py
+++ gpx.py
@@ -2,9 +2,6 @@
 
 Run with `uv run`
 
-TODO implement --pick filtering out previous
-  (see `git show 922df76fe9f14565e75b5e0fd4e2344f7a2590b4:gpx.ps1` for regex, but better to use actual date and Counter)
-
 Requires
   1. 1password op CLI logged in to my account
   2. strava_cli installed, `strava config` with https://www.strava.com/settings/api and `strava login`
@@ -27,7 +24,9 @@ import time
 import webbrowser
 import xml.etree.ElementTree as ET  # Not secure against DoS: for security use defusedxml or lxml
 from datetime import datetime, timedelta
+from functools import cache
 from pathlib import Path
+from typing import NamedTuple
 
 import questionary
 import requests
@@ -49,8 +48,17 @@ def strava_activity():
   if not args.pick:
     return next(client.get_activities(limit=1))
 
+  uploaded = set()
+  for gpx_file in gpx_files():
+    if m := re.match(r"Run \$?(\d{4}-\d{2}-\d{2}) looking for paths", gpx_file.description):
+      uploaded.add(m.group(1))
+
   choices = []
   for a in client.get_activities(limit=30):
+    if a.start_date_local.strftime("%Y-%m-%d") in uploaded:
+      # HACK if there are multiple activities on the same day, we'll skip all, but this is a good metric
+      continue
+
     # distance_quantity = a.distance.quantity()
     # HACK this is documented to work: https://stravalib.readthedocs.io/en/latest/reference/api/stravalib.model.Duration.html#stravalib.model.Duration
     # but it fails with AttributeError: 'float' object has no attribute 'quantity'
@@ -60,7 +68,7 @@ def strava_activity():
     sport = a.sport_type.root.rjust(7)[:7]
     name = a.name.ljust(50)[:50]  # TODO on windows, UTF-8 is getting messed up here
     dist = f"{distance_quantity.to('mile'):0.2f}s".rjust(11)
-    
+
     display = f"{a.start_date_local.replace(tzinfo=None)}   {sport}   {name}   {elapsed}   {dist}"
     choices.append({"name": display, "value": a})
 
@@ -95,8 +103,6 @@ def download_gpx(id):
 
 
 def upload_gpx(file: Path, start: datetime):
-  osm = get_osm()
-
   # https://wiki.openstreetmap.org/wiki/API_v0.6#Create:_POST_.2Fapi.2F0.6.2Fgpx.2Fcreate
   print(f"Using OSM API to upload {file}...")
   files = {"file": (file.name, file.read_bytes())}
@@ -105,36 +111,58 @@ def upload_gpx(file: Path, start: datetime):
     "tags": "",
     "visibility": "trackable",
   }
-  response = osm.post("gpx/create", files=files, data=data)
+  response = get_osm().post("gpx/create", files=files, data=data)
   response.raise_for_status()
   return int(response.text)
 
 
-def show_traces():
-  osm = get_osm()
-  # https://wiki.openstreetmap.org/wiki/API_v0.6#List:_GET_/api/0.6/user/gpx_files
-  response: requests.Response = osm.get("user/gpx_files")
-  response.raise_for_status()
+class GpxFile(NamedTuple):
+  id: str
+  name: str
+  user: str
+  visibility: str
+  timestamp: datetime
+  description: str
+  tags: list[str]
 
+
+def gpx_files() -> list[GpxFile]:
+  # https://wiki.openstreetmap.org/wiki/API_v0.6#List:_GET_/api/0.6/user/gpx_files
+  response: requests.Response = get_osm().get("user/gpx_files")
+  response.raise_for_status()
+  
   root = ET.fromstring(response.content)
+  return [
+    GpxFile(
+      id=gpx_file.get("id"),
+      name=gpx_file.get("name"), 
+      user=gpx_file.get("user"),
+      visibility=gpx_file.get("visibility"),
+      timestamp=datetime.strptime(gpx_file.get("timestamp"), "%Y-%m-%dT%H:%M:%SZ"),
+      description=gpx_file.find("description").text,
+      tags=[tag.text for tag in gpx_file.findall("tag")],
+    )
+    for gpx_file in root.findall("gpx_file")
+  ]
+
+
+def show_traces():
   traces = []
-  for gpx_file in root.findall("gpx_file"):
-    gpx_id = gpx_file.get("id")
-    name = gpx_file.get("name").removesuffix(".gpx")
-    description = gpx_file.find("description").text
-    if m := re.match(r"Run \$?(\d{4}-\d{2}-\d{2}) looking for paths", description):
+  for gpx_file in gpx_files():
+    name = gpx_file.name.removesuffix(".gpx")
+    if m := re.match(r"Run \$?(\d{4}-\d{2}-\d{2}) looking for paths", gpx_file.description):
       activity_date = datetime.strptime(m.group(1), "%Y-%m-%d")
     else:
-      upload_date = datetime.strptime(gpx_file.get("timestamp"), "%Y-%m-%dT%H:%M:%SZ")
-      activity_date = upload_date.replace(hour=0, minute=0, second=0, microsecond=0)  # Approximation
+      activity_date = gpx_file.timestamp.replace(hour=0, minute=0, second=0, microsecond=0)  # Approximation
 
-    text = f"{activity_date.strftime('%Y-%m-%d')} {name} https://www.openstreetmap.org/edit?gpx={gpx_id}"
+    text = f"{activity_date.strftime('%Y-%m-%d')} {name} https://www.openstreetmap.org/edit?gpx={gpx_file.id}"
     traces.append((activity_date, text))
   traces.sort()
   for _, description in traces:
     print(description)
 
 
+@cache
 def get_osm():
   osm_auth_op_id = "pihxghocs2meenfg4mqnpe433i"
   item = subprocess.check_output(["op", "item", "get", osm_auth_op_id, "--format", "json"], text=True)
