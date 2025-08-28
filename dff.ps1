256218e515c3099d7de0e0d9ef7b19a3500e111c
Carl Walsh <darthwalsh@gmail.com>
2025-08-28 13:03:55 -0700

    dff uses the clipboard as fallback for left/right

--- dff.ps1
+++ dff.ps1
@@ -4,6 +4,8 @@ Diff output of two script blocks
 .DESCRIPTION
 like bash process substitution:
     diff <(echo "abc") <(echo "def")
+If one script block is missing, the clipboard will be compared.
+If both script blocks are missing, the clipboard will be read twice.
 .PARAMETER Left
 Script block to run on the left side of diff
 .PARAMETER Right
@@ -13,9 +15,7 @@ PS> dff { code -h } { cursor -h }
 #>
 
 param(
-    [Parameter(Mandatory=$true)]
     [ScriptBlock] $Left,
-    [Parameter(Mandatory=$true)]
     [ScriptBlock] $Right
 )
 
@@ -26,8 +26,18 @@ Set-StrictMode -Version Latest
 $leftPath = New-TemporaryFile
 $rightPath = New-TemporaryFile
 
-& $Left > $leftPath
-& $Right > $rightPath
+if ($Left -and $Right) {
+  & $Left > $leftPath
+  & $Right > $rightPath
+} elseif ($Left) {
+  & $Left > $leftPath
+  Get-Clipboard > $rightPath
+} else {
+  Get-Clipboard > $leftPath
+  Read-Host "Press Enter when right side is on clipboard"
+  Get-Clipboard > $rightPath
+}
+
 
 try {
   git diff --no-index --word-diff=color --word-diff-regex=. $leftPath $rightPath
