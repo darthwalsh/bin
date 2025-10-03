a4ccc281a8a22903d6ce02c22592f258a4a89cf7
Carl Walsh <darthwalsh@gmail.com>
2025-10-03 14:26:58 -0700

    dff with -normalize and -unified

--- dff.ps1
+++ dff.ps1
@@ -10,13 +10,19 @@ If both script blocks are missing, the clipboard will be read twice.
 Script block to run on the left side of diff
 .PARAMETER Right
 Script block to run on the right side of diff
+.PARAMETER Normalize
+Script block to normalize the output of the script blocks
+.PARAMETER Unified
+Unified diff to remove context lines like: git diff -U0
 .EXAMPLE
-PS> dff { code -h } { cursor -h }
+PS> dff { code -h } { cursor -h } { $input | tr -d '[:space:]' | fold -w 160 }
 #>
 
 param(
     [ScriptBlock] $Left,
-    [ScriptBlock] $Right
+    [ScriptBlock] $Right,
+    [ScriptBlock] $Normalize=$null,
+    [switch] $Unified=$false
 )
 
 $script:ErrorActionPreference = "Stop"
@@ -26,6 +32,7 @@ Set-StrictMode -Version Latest
 $leftPath = New-TemporaryFile
 $rightPath = New-TemporaryFile
 
+# TODO allow left and right to be path as string or fileinfo objects
 if ($Left -and $Right) {
   & $Left > $leftPath
   & $Right > $rightPath
@@ -38,9 +45,23 @@ if ($Left -and $Right) {
   Get-Clipboard > $rightPath
 }
 
+if ($Normalize) {
+  $tmp = New-TemporaryFile # Can't read and write to the same file
+  cat $leftPath | & $Normalize > $tmp
+  mv $tmp $leftPath
+
+
+  cat $rightPath | & $Normalize > $tmp
+  mv $tmp $rightPath
+}
+
+$u0 = @()
+if ($Unified) {
+  $u0 += "-U0"
+}
 
 try {
-  git diff --no-index --word-diff=color --word-diff-regex=. $leftPath $rightPath
+  git diff --no-index --word-diff=color --word-diff-regex=. @u0 $leftPath $rightPath
 }
 catch {
 }
