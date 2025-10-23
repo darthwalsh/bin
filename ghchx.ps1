158a72fbe729d10266cb266c42605661569f77f8
Carl Walsh <darthwalsh@gmail.com>
2025-10-23 16:21:23 -0700

    ghchx working implementation

--- ghchx.ps1
+++ ghchx.ps1
@@ -1,6 +1,8 @@
 <#
 .SYNOPSIS
-WIP script for get github check status for PR, similar to gh pr checks $n
+Get github check status for PR
+.DESCRIPTION
+Similar to `gh pr checks $n` but opens links for required checks that are failing
 .PARAMETER n
 PR number
 .PARAMETER repo
@@ -20,19 +22,20 @@ Set-StrictMode -Version Latest
 if (-not $n) {
   throw "not implemented: getting current-branch PR"
 }
-if (-not $repo) {
-  $repo = '{owner}/{repo}'
+$repoArgs = if ($repo) {
+  $repoArgs = @("--repo", $repo)
 }
 
-$statusesUrl = gh api "/repos/$repo/pulls/$n" --jq .statuses_url
-$statuses = gh api $statusesUrl | ConvertFrom-Json
-
-# TODO get statues from gh pr checks 581 --required --json bucket,completedAt,description,event,link,name,startedAt,state,workflow
-# Was trying to use: gh pr view 581 --json statusCheckRollup --jq ".statusCheckRollup" might be better
-#   then maybe would check .conclusion or .state
-
-if ($action) {
-  $statuses | ? state -ne 'success'
-} else {
-  $statuses
+# https://cli.github.com/manual/gh_pr_checks
+$checks = gh @repoArgs pr checks $n --required --json 'name,bucket,link' | ConvertFrom-Json
+foreach ($check in $checks) {
+  $link = $check.link
+  if ($link.StartsWith('https://app.snyk.io/')) {
+    Write-Warning "Skipping Snyk, consider fixing: $link"
+    continue
+  }
+  $color = if ($check.bucket -in @("fail", "cancel")) { "Red" } elseif ($check.bucket -in @("success", "pass")) { "Green" } else { "Gray" }
+  Write-Host "Check $($check.name) $($check.bucket), see $link" -ForegroundColor $color
+  
+  if ($color -eq "Red") { open $link }
 }
