0e388e704ea1681aad7a17abfe09a134bd6d5258
Carl Walsh <darthwalsh@gmail.com>
2026-06-07 16:19:51 -0700

    deslop seems like a bad use of time

--- deslop.ps1
+++ deslop.ps1
@@ -6,6 +6,8 @@ Opens the most recently changed markdown "#ai-slop"
 $script:ErrorActionPreference = "Stop"
 Set-StrictMode -Version Latest
 
+throw "TODO not sure about deslop.ps1, but not a good use of time to just work on the latest one!"
+
 $notes = Join-Path (Get-Bin) apps/*.md
 $aiSlop = Get-ChildItem $notes | 
   Where-Object { Select-String '#ai-slop' $_ } |
@@ -14,6 +16,7 @@ $aiSlop = Get-ChildItem $notes |
 
 if ($aiSlop) {
   "Opening $aiSlop -- remove #ai-slop"
+  throw "TODO use obsidian CLI to open"
   open $aiSlop
 } else {
   echo "No slop to be found!"
